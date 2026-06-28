7b410f98e6f83622b3cc5ab7b83f4a321501d5bd
kavyasudhapadala <kavyasudha1427@gmail.com>
2026-06-28 17:58:57 +0530

    Fix filename case

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { PDFProvider } from "./context/PDFContext";
+import { PDFProvider } from "./context/PDFcontext";
 
 import App from "./App";
 import "./index.css";
